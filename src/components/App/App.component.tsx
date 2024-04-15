aa786322cea33f5ecb0aa8ad99b8f9329d2f78a2
Andrew Wittrock <andy@wittrock.us>
2024-04-14 23:38:52 -0500

    Add table of contents and scrollable view to Read component

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -5,7 +5,7 @@ import { AppProps } from "./App.d";
 import styles from "./App.module.scss";
 
 export const AppComponent = ({}:AppProps) => <div className={styles.app}>
-    <Typography.Text><h1>
+    <Typography.Text><h1 className={styles.mainHeader}>
         <img src="/PotA-icon.webp" width={64}/>
         Phantom of the Author-a&nbsp;
         <Tag>AI-powered Ghost Writer</Tag>
